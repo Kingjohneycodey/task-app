1c97b9c7a282f1162e6747532b46fe73b8f675e9
Kingjohneycodey <kvngjohnny10@gmail.com>
2024-05-18 14:41:30 +0100

    setup folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import './styles/layout.css'
+import './styles/main.css'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/user/Dashboard'
+import Layout from './components/Layout/Layout'
+import Tasks from './pages/user/Tasks'
+import NotFound from './pages/NotFound'
+import Login from './pages/auth/Login'
+import Signup from './pages/auth/Signup'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup/>} />
+
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tasks" element={<Tasks />} />
+        </Route>
+
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+
+    </Router>
   )
 }
 
---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+const api_url = import.meta.env.VITE_API_URL
+
+const token = localStorage.getItem('token')
